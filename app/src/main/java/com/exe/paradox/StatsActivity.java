783f60272988986f5aecbc02c4123661773ab8a5
octacode <allblue.shasha@gmail.com>
2018-03-23 16:42:38 +0530

    Added pie chart

--- app/src/main/java/com/exe/paradox/StatsActivity.java
+++ app/src/main/java/com/exe/paradox/StatsActivity.java
@@ -1,8 +1,14 @@
 package com.exe.paradox;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import az.plainpie.PieView;
+import az.plainpie.animation.PieAngleAnimation;
+import az.plainpie.animation.PieStrokeWidthAnimation;
 
 public class StatsActivity extends AppCompatActivity {
 
@@ -12,5 +18,21 @@ public class StatsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_stats);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_stats);
         setSupportActionBar(toolbar);
+
+        PieView pieView = (PieView) findViewById(R.id.pieView);
+        pieView.setInnerText("A");
+
+        PieAngleAnimation animation = new PieAngleAnimation(pieView);
+        animation.setDuration(2000); //This is the duration of the animation in millis
+        pieView.startAnimation(animation);
+        pieView.setPercentageBackgroundColor(Color.RED);
+        pieView.setPieInnerPadding(50);
+
+        pieView.setInnerTextVisibility(View.VISIBLE);
+
+        //fetch the level number and if 12 questions then 2/12
+        pieView.setInnerText("Level 9");
+
+        pieView.setPercentageTextSize(35);
     }
 }
